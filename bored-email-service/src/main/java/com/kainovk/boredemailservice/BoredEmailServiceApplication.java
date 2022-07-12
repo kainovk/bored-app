01775437c4f75d819131f64d35f05d8cc8b1a51f
Kirill Kainov <kainov.dev@gmail.com>
2022-07-12 09:55:18 +0300

    create main module

---
+++ bored-email-service/src/main/java/com/kainovk/boredemailservice/BoredEmailServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kainovk.boredemailservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoredEmailServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoredEmailServiceApplication.class, args);
+    }
+
+}
